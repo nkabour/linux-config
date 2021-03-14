1bca65f02d069f6512553cae7640025e2b42b2be
nkabour <nadak.k@live.com>
2021-03-14 22:16:07 +0300

    first commit' for my fabilous configs'

---
+++ .config/awesome/screen_layout.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#xrandr  --output DP-0 --primary  --output HDMI-0 --rotate right --right-of DP-0
+xrandr  --output DP-0 --primary  --output HDMI-0 --rotate right --left-of DP-0
