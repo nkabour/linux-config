1e57d386f82a27edc40b5269f8a605ad647fff31
nkabour <nadak.k@live.com>
2021-03-14 22:36:20 +0300

    added big directories by mistaku - fixed :p

--- .config/awesome/screen_layout.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#xrandr  --output DP-0 --primary  --output HDMI-0 --rotate right --right-of DP-0
-xrandr  --output DP-0 --primary  --output HDMI-0 --rotate right --left-of DP-0
